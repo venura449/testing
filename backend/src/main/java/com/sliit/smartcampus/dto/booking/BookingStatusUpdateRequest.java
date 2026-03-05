9b04c55b0f70defa76fdc7a543966e772ac247ec
Member2 <It23554818@my.sliit.lk>
2026-03-05 14:02:25 +0530

    Initial implementation of BookingStatusUpdateRequest.java

---
+++ backend/src/main/java/com/sliit/smartcampus/dto/booking/BookingStatusUpdateRequest.java
@@ -0,0 +1,8 @@
+package com.sliit.smartcampus.dto.booking;
+
+import com.sliit.smartcampus.entity.enums.BookingStatus;
+
+public record BookingStatusUpdateRequest(BookingStatus status, String reason) {
+}
+
+// pass 1
