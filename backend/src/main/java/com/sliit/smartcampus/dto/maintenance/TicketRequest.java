b01c9ca8b55e2f4502bffb60a0b30c9292d8f896
Member3 <lansakara1020@gmail.com>
2026-04-23 09:19:20 +0530

    Complete TicketRequest.java

---
+++ backend/src/main/java/com/sliit/smartcampus/dto/maintenance/TicketRequest.java
@@ -0,0 +1,8 @@
+package com.sliit.smartcampus.dto.maintenance;
+
+import com.sliit.smartcampus.entity.enums.TicketPriority;
+
+public record TicketRequest(String title, String description, TicketPriority priority) {
+}
+
+// pass 5
