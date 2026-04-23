84106a174506170e1bc8759d3e0c6f2401d6b05c
Member1 <thisalseniye@gmail.com>
2026-04-23 16:11:50 +0530

    Fix edge case in CampusResourceRepository.java

---
+++ backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
@@ -0,0 +1,9 @@
+package com.sliit.smartcampus.repository;
+
+import com.sliit.smartcampus.entity.CampusResource;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CampusResourceRepository extends MongoRepository<CampusResource, String> {
+}
+
+// pass 9
