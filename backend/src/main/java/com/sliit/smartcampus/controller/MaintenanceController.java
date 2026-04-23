ebd73f63d26aef0e96e4b1ba321aec060626b802
Member3 <lansakara1020@gmail.com>
2026-04-23 12:29:49 +0530

    Improve TicketRequest.java

--- backend/src/main/java/com/sliit/smartcampus/controller/MaintenanceController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/MaintenanceController.java
@@ -75,7 +75,7 @@ public class MaintenanceController {
     }
 
     @DeleteMapping("/comments/{commentId}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @PreAuthorize("isAuthenticated()")
     public void deleteComment(@PathVariable String commentId) {
         maintenanceService.deleteComment(commentId, currentUserService.requireCurrentUser());
